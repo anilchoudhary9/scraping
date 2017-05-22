b22d961bd6f3da686e3453c3d6cf254d8617e64c
anilchoudhary9 <meetanilchoudhary@gmail.com>
2017-05-22 15:21:59 +0530

    Add files via upload

---
+++ package.js
@@ -0,0 +1,36 @@
+{
+  "name": "Crawling",
+  "version": "1.0.0",
+  "description": "Crawl blogging website https://medium.com and find all possible hyperlinks present within https://medium.com website and generate output.",
+  "main": "index.js",
+  "scripts": {
+    "start": "node app.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "engines": {
+    "node": ">=6.2.2 <7.0.0"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/anilchoudhary9/crawling"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/anilchoudhary9/crawling/issues"
+  },
+  "homepage": "https://github.com/anilchoudhary9/crawling",
+  "devDependencies": {
+    "eslint": "3.1.1",
+    "eslint-config-airbnb": "9.0.1",
+    "eslint-plugin-import": "1.11.0",
+    "jscs": "3.0.7"
+  },
+  "dependencies": {
+    "bluebird": "3.4.1",
+    "debug": "2.2.0",
+    "lodash": "4.13.1",
+    "scraperjs": "1.2.0"
+  }
+}
